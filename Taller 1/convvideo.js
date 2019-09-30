dc61d956549a2e610862b84f95010d0f338d99e7
Arkai Ariza <arkaiariza@gmail.com>
2019-09-29 23:31:19 -0500

    Revert "Revert "a""
    This reverts commit ac27d54212c0dfd12da5c979dd5604622fef86f3.

--- Taller 1/convvideo.js
+++ Taller 1/convvideo.js
@@ -1,19 +1,22 @@
 var img;
-const convMatrix = [[1, 0, -1],[0, 0, 0],[-1, 0, 1]]
+const convMatrix = [[0, -1, 0], [-1, 5, -1], [0, -1, 0]];
 let convImg;
+let fps;
 
 var video;
 var convVideo;
 
-function preload(){
-    img = loadImage('https://ichef.bbci.co.uk/images/ic/768x432/p049tgdb.jpg');
-    video = createCapture(VIDEO);
-    video.size(640,480);
-    video.hide();
+function preload() {
+  img = loadImage('https://ichef.bbci.co.uk/images/ic/768x432/p049tgdb.jpg');
+  video = createCapture(VIDEO);
+  video.size(640, 480);
+  video.hide();
 }
 
 function setup() {
-  createCanvas(img.width*3, img.height*3);
+  fps = createGraphics(100, 100);
+
+  createCanvas(img.width * 2, img.height * 3);
   convImg = createGraphics(video.width, video.height);
   background(800);
   image(img, 0, video.height, img.width, img.height);
@@ -25,38 +28,48 @@ function setup() {
   image(convImg, img.width, video.height, img.width, img.height);
 }
 
-function draw(){
-  image(video, 0,0, video.width, video.height);
+function draw() {
+  image(video, 0, 0, video.width, video.height);
+
+  fps.fill(1);
+  fps.stroke(1);
+  fps.background(255);
+  fps.textSize(32);
+  fps.text(`fps: ${frameRate().toFixed(0)}`, 20, 60);
+  image(fps, 1280, 60);
+
   convVideo = video.get();
   convVideo.loadPixels();
   video.loadPixels();
+
   imageConvolution(convVideo, video);
   convVideo.updatePixels();
-  image(convVideo, video.width,0, video.width, video.height);
+  image(convVideo, video.width, 0, video.width, video.height);
 }
 
-function imageConvolution(dta, orig){
-  let size = 4*img.width*img.height;
+function imageConvolution(dta, orig) {
+  let size = 4 * img.width * img.height;
   for (let i = 0; i < size; i += 4) {
     dta.pixels[i] = updatedPixels(i, orig);
-    dta.pixels[i+1] = updatedPixels(i+1, orig);
-    dta.pixels[i+2] = updatedPixels(i+2, orig);
+    dta.pixels[i + 1] = updatedPixels(i + 1, orig);
+    dta.pixels[i + 2] = updatedPixels(i + 2, orig);
   }
-   
-};
-
-function updatedPixels(i, orig){
-  return (pixelConvoluted(i-4-orig.width*4,0,0, orig) +
-      pixelConvoluted(i-orig.width*4,0,1, orig) +
-      pixelConvoluted(i-4,1,0, orig) +
-      pixelConvoluted(i+orig.width*4,2,1, orig) +
-      pixelConvoluted(i+4,1,2, orig) +
-      pixelConvoluted(i,1,1, orig) +
-      pixelConvoluted(i+4+orig.width*4,2,2, orig) +
-      pixelConvoluted(i-4+orig.width*4,2,0, orig) +
-      pixelConvoluted(i+4-orig.width*4,0,2, orig))
 }
 
-function pixelConvoluted(i, x, y, orig){
-  return orig.pixels[i]*convMatrix[x][y]
+function updatedPixels(i, orig) {
+  return (
+    pixelConvoluted(i - 4 - orig.width * 4, 0, 0, orig) +
+    pixelConvoluted(i - orig.width * 4, 0, 1, orig) +
+    pixelConvoluted(i - 4, 1, 0, orig) +
+    pixelConvoluted(i + orig.width * 4, 2, 1, orig) +
+    pixelConvoluted(i + 4, 1, 2, orig) +
+    pixelConvoluted(i, 1, 1, orig) +
+    pixelConvoluted(i + 4 + orig.width * 4, 2, 2, orig) +
+    pixelConvoluted(i - 4 + orig.width * 4, 2, 0, orig) +
+    pixelConvoluted(i + 4 - orig.width * 4, 0, 2, orig)
+  );
+}
+
+function pixelConvoluted(i, x, y, orig) {
+  return orig.pixels[i] * convMatrix[x][y];
 }
